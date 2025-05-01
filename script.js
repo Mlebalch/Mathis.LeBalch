029c329372eff0950ad9b492a82e7df4036b9141
lebalch <mathis.le-balch@etu.umontpellier.fr>
2025-05-01 20:25:26 +0200

    update portfolio

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+
+// Gestion du menu burger
+const burgerMenu = document.querySelector('.burger-menu');
+const navLinks = document.querySelector('.nav-links');
+
+burgerMenu.addEventListener('click', () => {
+    burgerMenu.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
+
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        burgerMenu.classList.remove('active');
+        navLinks.classList.remove('active');
+    });
+});
+
+
+document.addEventListener('click', (e) => {
+    if(!navLinks.contains(e.target) && !burgerMenu.contains(e.target)) {
+        burgerMenu.classList.remove('active');
+        navLinks.classList.remove('active');
+    }
+});
+
+
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     // Gestion du thème
     const themeToggle = document.getElementById('theme-toggle');
